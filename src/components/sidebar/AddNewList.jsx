d088e93079316da4877824eb8060c41caaa8ab5f
Abdulaziz <abdulazizdebo@gmail.com>
2025-04-18 02:35:40 +0200

    Edit AddnewList.jsx

--- src/components/sidebar/AddNewList.jsx
+++ src/components/sidebar/AddNewList.jsx
@@ -11,19 +11,19 @@ export default function AddNewList(){
     const {dispatch} = useLists();
     const [selectedColor, setSelectedColor] = useState('#5C7CFA');
     const [newListTitle, setNewListTitle] = useState('');
-    function HandleSetLists(newList){
+    function HandleAddList(newList){
         dispatch({type: ACTIONS.ADD, payload: newList})
     }
     return(
         <div className="add-list">
             <div className="flex gap-5">
                 <CustomeInput onChange={setNewListTitle} value={newListTitle} onEnter={() => {
-                    HandleSetLists({title: newListTitle, iconColor:selectedColor});
+                    HandleAddList({title: newListTitle, iconColor:selectedColor});
                     setNewListTitle("");
                     setSelectedColor('#5C7CFA');
                     }} icon={<ColoredIcon style={{backgroundColor:selectedColor}} />} placeholder={'List Name'}/>
                 <Button onClick={() => {
-                    HandleSetLists({title: newListTitle, iconColor:selectedColor});
+                    HandleAddList({title: newListTitle, iconColor:selectedColor});
                     setNewListTitle("");
                     setSelectedColor('#5C7CFA');
                     }} title={<AddIcon />} style={{width: '50px'}} />
